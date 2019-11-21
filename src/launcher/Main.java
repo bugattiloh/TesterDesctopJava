872068207f296df174ae5c6e0cbb602c95841e92
bugattiloh <bugattiloh2000@gmail.com>
2019-11-21 21:54:38 +0300

    Fuck Zamanov

--- src/launcher/Main.java
+++ src/launcher/Main.java
@@ -7,8 +7,14 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+
     public static void main(String[] args) {
+        createApplicationForTestingFuckingPeople();
         launch(args);
+    }
+
+    private static void createApplicationForTestingFuckingPeople() {
+
     }
 
     @Override
